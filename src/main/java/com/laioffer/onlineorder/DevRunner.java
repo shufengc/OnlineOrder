8d3838ba79d45f53df9292f2860c8e5055af6b82
Hammer <hn2477@columbia.edu>
2025-11-23 16:21:06 -0500

    Add files via upload

--- src/main/java/com/laioffer/onlineorder/DevRunner.java
+++ src/main/java/com/laioffer/onlineorder/DevRunner.java
@@ -24,18 +24,14 @@ import java.util.List;
 
 @Component
 public class DevRunner implements ApplicationRunner {
-
-    // Logger for printing test results
     private static final Logger logger = LoggerFactory.getLogger(DevRunner.class);
 
-    // repositories
     private final CartRepository cartRepository;
     private final CustomerRepository customerRepository;
     private final MenuItemRepository menuItemRepository;
     private final OrderItemRepository orderItemRepository;
     private final RestaurantRepository restaurantRepository;
 
-    // services
     private final CartService cartService;
     private final MenuItemService menuItemService;
     private final RestaurantService restaurantService;
@@ -63,55 +59,58 @@ public class DevRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        // ----------------------------------------------------
-        // 1. Insert test data using repositories
-        // ----------------------------------------------------
-
-        // customers
-        CustomerEntity customer0 =
-                new CustomerEntity(null, "user_a@mail.com", "123", true, "Foo", "Bar");
-        CustomerEntity customer1 =
-                new CustomerEntity(null, "user_b@mail.com", "123", true, "Zoo", "Yoo");
-
+        // Insert customers
+        CustomerEntity customer0 = new CustomerEntity(
+                null, "user_a@mail.com", "123", true, "Foo", "Bar");
         customerRepository.save(customer0);
+
+        CustomerEntity customer1 = new CustomerEntity(
+                null, "user_b@mail.com", "123", true, "Zoo", "Yoo");
         customerRepository.save(customer1);
 
-        // carts
+        // Insert carts
         cartRepository.saveAll(List.of(
                 new CartEntity(null, 1L, 0.0),
                 new CartEntity(null, 2L, 0.0)
         ));
 
-        // restaurants
+        // Insert restaurants
         List<RestaurantEntity> newRestaurants = List.of(
-                new RestaurantEntity(null, "restaurant_a", "address_a", "6505550000", "https://001.jpg"),
-                new RestaurantEntity(null, "restaurant_b", "address_b", "6505550001", "https://002.jpg"),
-                new RestaurantEntity(null, "restaurant_c", "address_c", "6505550002", "https://003.jpg")
+                new RestaurantEntity(null, "restaurant_a", "address_a",
+                        "6505550000", "https://001.jpg"),
+
+                new RestaurantEntity(null, "restaurant_b", "address_b",
+                        "6505550001", "https://002.jpg"),
+
+                new RestaurantEntity(null, "restaurant_c", "address_c",
+                        "6505550002", "https://003.jpg")
         );
         restaurantRepository.saveAll(newRestaurants);
 
-        // menu items
+        // Insert menu items
         List<MenuItemEntity> newMenuItems = List.of(
                 new MenuItemEntity(null, 4L, "entree_aa", "description_aa", 10.0, "https://001.jpg"),
                 new MenuItemEntity(null, 4L, "entree_ab", "description_ab", 20.0, "https://002.jpg"),
                 new MenuItemEntity(null, 4L, "entree_ac", "description_ac", 33.3, "https://003.jpg"),
+
                 new MenuItemEntity(null, 5L, "entree_ba", "description_ba", 1.0, "https://004.jpg"),
                 new MenuItemEntity(null, 5L, "entree_bb", "description_bb", 2.0, "https://005.jpg"),
-                new MenuItemEntity(null, 5L, "entree_bc", "description_bc", 100.0, "https://006.jpg"),
-                new MenuItemEntity(null, 6L, "entree_ca", "description_ca", 20.0, "https://007.jpg"),
-                new MenuItemEntity(null, 6L, "entree_cb", "description_cb", 88.8, "https://008.jpg"),
-                new MenuItemEntity(null, 6L, "entree_cc", "description_cc", 33.3, "https://009.jpg")
+                new MenuItemEntity(null, 5L, "entree_bc", "description_bc", 3.3, "https://006.jpg"),
+
+                new MenuItemEntity(null, 6L, "entree_ca", "description_ca", 100.0, "https://007.jpg"),
+                new MenuItemEntity(null, 6L, "entree_cb", "description_cb", 200.0, "https://008.jpg"),
+                new MenuItemEntity(null, 6L, "entree_cd", "description_cc", 333.3, "https://009.jpg")
         );
         menuItemRepository.saveAll(newMenuItems);
 
-        // order items
         List<OrderItemEntity> newOrderItems = List.of(
                 new OrderItemEntity(null, 1L, 1L, 1.0, 1),
                 new OrderItemEntity(null, 3L, 1L, 10.0, 1),
                 new OrderItemEntity(null, 5L, 1L, 20.0, 1),
                 new OrderItemEntity(null, 7L, 1L, 200.0, 1),
                 new OrderItemEntity(null, 9L, 1L, 30.0, 1),
-                new OrderItemEntity(null, 2L, 2L, 1.0, 1),
+
+                new OrderItemEntity(null, 1L, 2L, 1.0, 1),
                 new OrderItemEntity(null, 2L, 2L, 10.0, 1),
                 new OrderItemEntity(null, 3L, 2L, 20.0, 1),
                 new OrderItemEntity(null, 4L, 2L, 200.0, 1),
@@ -119,22 +118,15 @@ public class DevRunner implements ApplicationRunner {
         );
         orderItemRepository.saveAll(newOrderItems);
 
-        // some delete / update examples
         customerRepository.deleteById(2L);
         restaurantRepository.deleteById(4L);
         customerRepository.updateNameByEmail("user_a@mail.com", "first", "last");
 
-        // ----------------------------------------------------
-        // 2. Call service APIs and log the results
-        // ----------------------------------------------------
-
-        // RestaurantService: get all restaurants with their menu items
         List<RestaurantDto> restaurantDtos = restaurantService.getRestaurants();
         logger.info("Restaurant DTOs: {}", restaurantDtos);
 
         // MenuItemService: get menu items by restaurant id
-        List<MenuItemEntity> menuItemEntities =
-                menuItemService.getMenuItemsByRestaurantId(2L);
+        List<MenuItemEntity> menuItemEntities = menuItemService.getMenuItemsByRestaurantId(2L);
         logger.info("Menu items of restaurant 2: {}", menuItemEntities);
 
         // MenuItemService: get single menu item
@@ -148,10 +140,8 @@ public class DevRunner implements ApplicationRunner {
         cartService.addMenuItemToCart(1L, 5L);
         cartService.addMenuItemToCart(1L, 5L);
 
-        logger.info("Cart of customer 1 (after adding items): {}", cartService.getCart(1L).toString());
-
-        // clear cart 1
+        logger.info(cartService.getCart(1L).toString());
         cartService.clearCart(1L);
-        logger.info("Cart of customer 1 (after clear): {}", cartService.getCart(1L).toString());
+        logger.info(cartService.getCart(1L).toString());
     }
 }
