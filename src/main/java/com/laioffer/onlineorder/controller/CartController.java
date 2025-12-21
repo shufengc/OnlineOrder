5ce81535c3dcc0cc1804449dedd15f6af2a30979
Hammer <hn2477@columbia.edu>
2025-12-20 21:23:54 -0500

    Add files via upload

--- src/main/java/com/laioffer/onlineorder/controller/CartController.java
+++ src/main/java/com/laioffer/onlineorder/controller/CartController.java
@@ -7,10 +7,9 @@ import com.laioffer.onlineorder.service.CartService;
 import com.laioffer.onlineorder.service.CustomerService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class CartController {
@@ -49,4 +48,14 @@ public class CartController {
                 customerService.getCustomerByEmail(user.getUsername());
         cartService.clearCart(customer.id());
     }
+
+    @DeleteMapping("/cart/items/{orderItemId}")
+    public void removeMenuItemFromCart(
+            @AuthenticationPrincipal User user,
+            @PathVariable long orderItemId
+    ) {
+        CustomerEntity customer = customerService.getCustomerByEmail(user.getUsername());
+        cartService.removeMenuItemFromCart(customer.id(), orderItemId);
+    }
 }
+
